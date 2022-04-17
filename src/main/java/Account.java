9835d07c4d32a4c90b29e4d565fa43d55f8696b8
Aura Russill <auracris1996@hotmail.com>
2022-04-17 16:01:05 -0500

    Creacion del metodo transfer

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -3,24 +3,37 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 public class Account {
+    private String addressee;
+    private int balance;
     HashMap<LocalDate, Integer> mapDeposit = new HashMap<>();
     HashMap<LocalDate, Integer> mapWithdraw = new HashMap<>();
+    HashMap<LocalDate, Integer> mapTransfer = new HashMap<>();
+    HashMap<String, Object> mapStatus = new HashMap<>();
 
     void deposit(int amount, String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate day = LocalDate.parse(date, formatter);
         mapDeposit.put(day, amount);
+        mapStatus.put("deposit", mapDeposit);
     }
 
     void withdraw(int amount, String date){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         LocalDate day = LocalDate.parse(date, formatter);
         mapWithdraw.put(day, amount);
+        mapStatus.put("withdraw", mapWithdraw);
+    }
+
+    void transfer(int amount, String date, String number){
+        addressee = number;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate day = LocalDate.parse(date, formatter);
+        mapTransfer.put(day, amount);
+        mapStatus.put("transfer", mapTransfer);
     }
 
     void printStatements() {
-        System.out.println(mapDeposit);
-        System.out.println(mapWithdraw);
+        System.out.println(mapStatus);
     }
 
 }

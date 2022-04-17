e690438e35d749ee6e324878cc1ed766ba88a796
Aura Russill <auracris1996@hotmail.com>
2022-04-17 14:11:17 -0500

    Creacion de main en StartApp

---
+++ src/main/java/StartApp.java
@@ -0,0 +1,13 @@
+import java.sql.Statement;
+
+public class StartApp {
+    public static void main(String[] args){
+        Account account = new Account(new Statement());
+
+        account.deposit(amountOf(1000), date("10/01/2021"));
+        account.deposit(amountOf(2000), date("13/01/2021"));
+        account.withdraw(amountOf(500), date("14/01/2021"));
+
+        accont.printStament(System.out);
+    }
+}

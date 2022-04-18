d69ab74bf110c3e3b50bc8ae08c979e89767b2df
Aura Russill <auracris1996@hotmail.com>
2022-04-17 20:44:02 -0500

    add code metodo printStatements

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,39 +1,51 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class Account {
-    private String addressee;
+//    private String addressee;
     private int balance;
     HashMap<LocalDate, Integer> mapDeposit = new HashMap<>();
     HashMap<LocalDate, Integer> mapWithdraw = new HashMap<>();
-    HashMap<LocalDate, Integer> mapTransfer = new HashMap<>();
-    HashMap<String, Object> mapStatus = new HashMap<>();
+//    HashMap<LocalDate, Integer> mapTransfer = new HashMap<>();
+    HashMap<LocalDate, Integer> mapStatus = new HashMap<>();
 
     void deposit(int amount, String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate day = LocalDate.parse(date, formatter);
+        LocalDate day = formatterDate(date);
+        balance += amount;
         mapDeposit.put(day, amount);
-        mapStatus.put("deposit", mapDeposit);
+        mapStatus.put(day, balance);
     }
 
     void withdraw(int amount, String date){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate day = LocalDate.parse(date, formatter);
+        LocalDate day = formatterDate(date);
+        balance -= amount;
         mapWithdraw.put(day, amount);
-        mapStatus.put("withdraw", mapWithdraw);
+        mapStatus.put(day, balance);
     }
 
-    void transfer(int amount, String date, String number){
-        addressee = number;
+//    void transfer(int amount, String date, String number){
+//        addressee = number;
+//        LocalDate day = formateaDate(date);
+//        mapTransfer.put(day, amount);
+//        mapStatus.put("transfer", mapTransfer);
+//    }
+
+    private LocalDate formatterDate(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate day = LocalDate.parse(date, formatter);
-        mapTransfer.put(day, amount);
-        mapStatus.put("transfer", mapTransfer);
+        return LocalDate.parse(date, formatter);
     }
 
     void printStatements() {
-        System.out.println(mapStatus);
+        mapStatus.forEach((k,v) -> {
+            if(mapDeposit.containsKey(k)){
+                System.out.println("Fecha: " + k + "| Debito: " + mapDeposit.getOrDefault(k,0) + "| Balance " + v );
+            }else{
+                System.out.println("Fecha: " + k + "| Credito: " + mapWithdraw.getOrDefault(k,0) + "| Balance " + v );
+            }
+        });
+
     }
 
 }
--- src/main/java/StartApp.java
+++ src/main/java/StartApp.java
@@ -2,7 +2,7 @@ public class StartApp {
     public static void main(String[] args){
         Account account = new Account();
 
-        account.deposit(100, "10/01/2021");
+        account.deposit(1000, "10/01/2021");
         account.deposit(2000, "13/01/2021");
         account.withdraw(500, "14/01/2021");
 

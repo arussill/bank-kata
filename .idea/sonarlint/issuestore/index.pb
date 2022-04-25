81cbb53219a2818f92452111cb4c3fb18deb441d
Aura Russill <auracris1996@hotmail.com>
2022-04-24 19:28:10 -0500

    REFACTOR & DELECT funcion de tran

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+Q
+!src/test/java/AcceptanceTest.java,5\c\5c73d05a86164673933bb2e0744d2a7f032ce36e
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+K
+src/main/java/StartApp.java,8\b\8b14bd8b06cbe366237f9e7a238172c3ebe3d9c0
+J
+src/main/java/Account.java,d\9\d972e435e5895fc8da97f34cf6c4cf52b872e785
--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,36 +1,27 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.Objects;
 
 public class Account {
 //    private String addressee;
-    private int balance;
-    HashMap<LocalDate, Integer> mapDeposit = new HashMap<>();
-    HashMap<LocalDate, Integer> mapWithdraw = new HashMap<>();
-//    HashMap<LocalDate, Integer> mapTransfer = new HashMap<>();
+    private Integer balance;
+    private HashMap<LocalDate, Integer> mapDeposit = new HashMap<>();
+    private HashMap<LocalDate, Integer> mapWithdraw = new HashMap<>();
     HashMap<LocalDate, Integer> mapStatus = new HashMap<>();
 
-    void deposit(int amount, String date) {
+    void deposit(Integer amount, String date) {
         LocalDate day = formatterDate(date);
         balance += amount;
         mapDeposit.put(day, amount);
         mapStatus.put(day, balance);
     }
 
-    void withdraw(int amount, String date){
+    void withdraw(Integer amount, String date){
         LocalDate day = formatterDate(date);
         balance -= amount;
         mapWithdraw.put(day, amount);
         mapStatus.put(day, balance);
     }
-
-//    void transfer(int amount, String date, String number){
-//        addressee = number;
-//        LocalDate day = formateaDate(date);
-//        mapTransfer.put(day, amount);
-//        mapStatus.put("transfer", mapTransfer);
-//    }
 
     private LocalDate formatterDate(String date) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
